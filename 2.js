a78b79e90b27ffe7f1dcc12fcb694ef2beb09f1e
Patnala Maheshwar <23071a66b6@vnrvjiet.in>
2024-12-28 16:00:19 +0530

    Version 1

---
+++ 2.js
@@ -0,0 +1,17 @@
+//variable declaration... cannot determine the datatype and default is undefined.
+
+let a;
+console.log("V1");
+console.log(a)
+
+a = 10;
+console.log(a)
+
+a = 'Hello'
+console.log(a)
+
+a = true
+console.log(a)
+
+//dynamically typed... any kind od data can be solved
+//typescript
---
+++ Assignment-1(JS)(1).js
@@ -0,0 +1,36 @@
+//Arithmetic Operations
+let a = 22;
+let b = 10;
+console.log('Artihmetic Operations')
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+
+//Unary Increment and decrement
+let c = 3;
+console.log('Increment and decrement')
+console.log(c)
+c++
+console.log(c)
+c--
+console.log(c)
+
+//Comparision operators
+let d = 30;
+let e = 22;
+console.log('Comparision operators')
+console.log(d>e)
+console.log(e>d)
+console.log(e==d)
+
+//Difference between == and ===
+let f = true;
+let g = 1;
+console.log('== ===')
+console.log(f==g)
+console.log(f===g)
+//=== is a strict comparision operator, whereas == tries to convert any value into one datatype to compare.
+
+/******************************************************************************************************************/
